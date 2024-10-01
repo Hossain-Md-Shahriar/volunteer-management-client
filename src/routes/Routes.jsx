03a5f3767734366126a35cb63690a7739726f1e5
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-10-01 11:56:55 +0600

    setup done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Main from "../layouts/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
