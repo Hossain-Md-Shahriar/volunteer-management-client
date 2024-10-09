bc9a8f6a8f6ea5188154d8c1633f9d8e73bf32e7
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-10-09 19:30:44 +0600

    functionality added to add volunteer post page

--- src/pages/AddVolunteerPost.jsx
+++ src/pages/AddVolunteerPost.jsx
@@ -1,7 +1,206 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import useAuth from "../hooks/useAuth";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 const AddVolunteerPost = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const thumbnail = form.thumbnail.value;
+    const postTitle = form.postTitle.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const location = form.location.value;
+    const volunteersNeeded = parseInt(form.volunteersNeeded.value);
+    const deadline = startDate;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    const needVolunteerData = {
+      thumbnail,
+      postTitle,
+      description,
+      category,
+      location,
+      volunteersNeeded,
+      deadline,
+      organizer: {
+        name,
+        email,
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/need-volunteer`,
+        needVolunteerData
+      );
+      console.log(data);
+      toast.success("Post Added Successfully!");
+      navigate("/manage-my-post");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  };
+
   return (
-    <div>
-      <h2 className="text-2xl">add volunteer post</h2>
+    <div className="my-10">
+      <section className="max-w-2xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Add Volunteer Post
+        </h2>
+
+        <form onSubmit={handleFormSubmit}>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="thumbnail"
+              >
+                Thumbnail
+              </label>
+              <input
+                id="thumbnail"
+                name="thumbnail"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="postTitle"
+              >
+                Post Title
+              </label>
+              <input
+                id="postTitle"
+                name="postTitle"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label className="text-gray-700 " htmlFor="description">
+                Description
+              </label>
+              <textarea
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                id="description"
+                name="description"
+              ></textarea>
+            </div>
+
+            <div className="flex flex-col gap-2 ">
+              <label className="text-gray-700 " htmlFor="category">
+                Category
+              </label>
+              <select
+                id="category"
+                name="category"
+                className="border p-2 rounded-md"
+              >
+                <option value="Healthcare">Healthcare</option>
+                <option value="Education">Education</option>
+                <option value="Social Service">Social Service</option>
+                <option value="Animal Welfare">Animal Welfare</option>
+              </select>
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="location"
+              >
+                Location
+              </label>
+              <input
+                id="location"
+                name="location"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="volunteersNeeded"
+              >
+                No. of Volunteers Needed
+              </label>
+              <input
+                id="volunteersNeeded"
+                name="volunteersNeeded"
+                type="number"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label className="text-gray-700">Deadline</label>
+
+              {/* Date Picker Input Field */}
+              <DatePicker
+                className="border p-2 rounded-md w-full"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="name"
+              >
+                Organizer Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                defaultValue={user?.displayName}
+                disabled
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="email"
+              >
+                Organizer email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                defaultValue={user?.email}
+                disabled
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-end mt-6">
+            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Add Post
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 };
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import LoadingState from "../components/LoadingState";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
   const location = useLocation();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <LoadingState />;
   if (user) return children;
 
   return <Navigate to="/login" state={location.pathname} replace={true} />;
